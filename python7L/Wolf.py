3e915c16cee905f4aaa897c61dc64a56fc6f339e
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-02-03 01:11:07 -1000

    givu ting

---
+++ python7L/Wolf.py
@@ -0,0 +1,17 @@
+import os
+from time import sleep
+from os import *
+TitleZero=('title MyDogs')
+os.system(TitleZero)
+os.system("color 01")
+os.system("mode con: cols=55 lines=11")
+cls = ('cls')
+os.system(cls)
+print("By Kia7L: Chippa/Melekama")
+print("Type Name Only")
+a=(raw_input("GameFolder?RootName! : "))
+c=('C:\\Windows\\nircmd.exe shellcopy .\\Chipa\\*.* '+a)
+os.system(c)
+print ("Coppoas wolf wolf bark bark woo wooh")
+sleep(8)
+
