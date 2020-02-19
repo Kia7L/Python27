364cc1a50b4e08ac3a8ccfc281e61f859ca7fe8c
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-02-18 22:38:20 -1000

    TempFixBandaide

---
+++ python7L/4.py
@@ -0,0 +1,15 @@
+import os
+from os import *
+TitleZero=('title EXE.PY')
+os.system('TitleZero')
+os.system("color 01")
+os.system("mode 66,11")
+os.system("cd \\")
+cls = 'cls'
+os.system(cls)
+print("By Kia7L: PY ")
+copyer=('copy /Y ')
+path=(".\\4.txt ")
+path2=(".\\V2.inf")
+os.system(copyer+path+path2)
+ 
